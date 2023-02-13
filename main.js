1fd54f279d025fac195dc9a72e8d4ccdc029324b
ByBeta1 <100860273+ByBeta1@users.noreply.github.com>
2023-02-13 07:56:58 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+function holamundo ()
+{
+console.log("holamundo")
+
+}
+
+
+
+/*
+FUNCIONAL
+
+let dato1, dato2, num1, num2;
+ dato1 = window.prompt("Introduce primer número ?", "0");
+ num1 = parseInt(dato1);
+ dato2 = window.prompt("Introduce segundo número ?", "0");
+ num2 = parseInt(dato2);
+ let resultado = num1 + num2;
+ document.write(`<br/> <br/> La suma es ${resultado} ` );
+*/
+/*
+
+IMPERATIVA
+
+
+var people = [
+  {
+    name: 'Aang',
+    age: 12,
+  },
+  {
+    name: 'Sokka',
+    age: 15,
+  },
+  {
+    name: 'Toph',
+    age: 12,
+  },
+  {
+    name: 'Iroh',
+    age: 55,
+  }
+];
+var kid_names = [];
+for (let person of people) {
+  if (person.age <= 12) {
+    kid_names.push(person.name);
+  }
+}
+console.log(kid_names);
+
+*/
+
+
+
+
+
+
+
+
+
